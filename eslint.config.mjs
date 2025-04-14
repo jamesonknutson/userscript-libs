d94a86f7d7afbde0c6f0df39ef84c4d6c91bce20
Jameson Knutson <jamesonknutson@me.com>
2025-04-14 03:09:09 -0700

    add createLogger

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,14 @@ import css from "@eslint/css";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], rules: {
+    'no-unused-vars': 'off'
+  } },
   { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {
+    ...globals.browser,
+    ...globals.greasemonkey,
+  } } },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
   { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
